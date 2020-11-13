3e12e4c0f9ca979c9ccedf70ca5069896d173f02
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2020-11-13 17:34:59 +0530

    high score updated with local storage

--- JS/end.js
+++ JS/end.js
@@ -3,8 +3,12 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup",() =>{
     saveScoreBtn.disabled = !username.value;
 });
@@ -13,4 +17,18 @@ username.addEventListener("keyup",() =>{
 saveHighScore = e =>{
     console.log("clicked the save button");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+    highScores.sort((a,b) =>{
+        return b.score > a.score
+    });
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem("highScores" , JSON.stringify(highScores));
+    window.location.assign("../index.html");
 }; 
